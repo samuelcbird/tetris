60b817bf7a89e57843bd4d935ed6b0632febe64b
SBird <samuel@samuelcbird.co.uk>
2020-02-21 13:23:06 +0000

    add preview tetromino window

--- tetris.py
+++ tetris.py
@@ -3,17 +3,16 @@ import random
 
 
 class SetupGame:
+    """ This class manages the all game events and configures Pygame. """
 
     def __init__(self):
         pygame.init()
 
         self.clock = pygame.time.Clock()
         self.block = 40
-        self.main_window_width_height = (20*self.block, 20*self.block)
+        self.main_window_width_height = (22*self.block, 20*self.block)
         self.main_window = pygame.display.set_mode(self.main_window_width_height)
         pygame.display.set_caption("Tetris")
-
-        self.draw_array = []
 
         # define colours
         self.dark_blue, self.light_blue = (40, 63, 128), (156, 182, 255)
@@ -22,20 +21,49 @@ class SetupGame:
         self.dark_orange, self.light_orange = (204, 65, 18), (255, 138, 99)
         self.default_grey = (192, 192, 192)
         self.off_white = (240, 240, 240)
+        self.dark_grey = (10, 10, 10)
+        self.colour_list = [(self.dark_blue, self.light_blue), (self.dark_purple, self.light_purple), (self.dark_pink,
+                            self.light_pink), (self.dark_orange, self.light_orange)]
 
+        # create surfaces for different displays
         self.main_window.fill(self.default_grey)
         self.game_display = pygame.Surface((10*self.block, 18*self.block))
         self.game_display.fill((240, 240, 240))
+        self.next_tetromino_display = pygame.Surface((4*self.block, 5*self.block))
+        self.next_tetromino_display.fill(self.dark_grey)
+
+        # gameplay variables
+        self.current_tetromino = None
+        self.next_tetromino = None
+        self.static_tetrominoes = pygame.sprite.Group()
+        self.gameplay()
 
     def loop(self):
+        self.event_handling()
         pygame.display.update()
-        self.main_window.blit(self.game_display, (5*self.block, 1*self.block))
+        self.main_window.blit(self.game_display, (6*self.block, 1*self.block))
+        self.main_window.blit(self.next_tetromino_display, (17*self.block, 1*self.block))
 
         self.game_display.fill(self.off_white)
+        self.next_tetromino_display.fill(self.dark_grey)
+
+        self.current_tetromino.draw(self.game_display)
+        # self.current_tetromino.gravity()
+        self.next_tetromino.draw(self.next_tetromino_display)
+
         self.clock.tick(60)
 
-    @staticmethod
-    def event_handling():
+    def gameplay(self):
+        if self.current_tetromino is None:
+            self.current_tetromino = Tetromino(self.colour_list)
+            self.current_tetromino.next_tetromino = False
+            self.current_tetromino.current_tetromino = True
+            self.current_tetromino.set_play_coordinates()
+            self.next_tetromino = Tetromino(self.colour_list)
+            # while self.current_tetromino is not None:
+            #     pass
+
+    def event_handling(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (
                     event.type == pygame.KEYDOWN and (
@@ -43,7 +71,8 @@ class SetupGame:
                 pygame.quit()
                 quit()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                tet.rotate()
+                # todo - configure space bar rotation
+                self.current_tetromino.rotate()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
                 pass
             elif event.type == pygame.MOUSEBUTTONUP:
@@ -51,6 +80,7 @@ class SetupGame:
 
 
 class Block(pygame.sprite.Sprite):
+    """ Creates one of the four 39*39 pixel blocks which a Tetromino is made up of. """
 
     def __init__(self, colour, identification):
         pygame.sprite.Sprite.__init__(self)
@@ -70,20 +100,20 @@ class Block(pygame.sprite.Sprite):
 
 
 class Tetromino(pygame.sprite.Sprite):
+    """ By creating four instances of Block this class builds the random Tetromino. """
 
-    def __init__(self):
+    def __init__(self, colours):
         pygame.sprite.Sprite.__init__(self)
         self.block_size = 40
         self.image = pygame.Surface([160, 160])
         self.rect = self.image.get_rect()
-        self.colour = random.choice([(new_game.dark_blue, new_game.light_blue),
-                                     (new_game.dark_pink, new_game.light_pink),
-                                     (new_game.dark_purple, new_game.light_purple),
-                                     (new_game.dark_orange, new_game.light_orange)])
+        self.colour = random.choice(colours)
+        self.next_tetromino = True
+        self.current_tetromino = False
 
-        # get random x coordinate
-        self.rect.x = random.choice(range(0, 8*self.block_size, self.block_size))
-        self.rect.y = 20
+        # set co-ordinates for preview box
+        self.rect.x = -20
+        self.rect.y = -20
         self.blocks = pygame.sprite.Group()
         self.create_blocks()
         # holds the current rotation
@@ -112,9 +142,9 @@ class Tetromino(pygame.sprite.Sprite):
                                         "-----",
                                         "-----"]],
                                  "J": [["-----",
-                                        "--B--",
-                                        "--B--",
-                                        "-BB--",
+                                        "---B-",
+                                        "---B-",
+                                        "--BB-",
                                         "-----"],
                                        ["-----",
                                         "-----",
@@ -142,9 +172,9 @@ class Tetromino(pygame.sprite.Sprite):
                                         "-BBBB",
                                         "-----"]],
                                  "T": [["-----",
-                                        "-----",
                                         "-BBB-",
                                         "--B--",
+                                        "-----",
                                         "-----"],
                                        ["-----",
                                         "--B--",
@@ -182,8 +212,8 @@ class Tetromino(pygame.sprite.Sprite):
                                         "-----",
                                         "-----"]],
                                  "O": [["-----",
-                                        "--BB-",
-                                        "--BB-",
+                                        "-BB--",
+                                        "-BB--",
                                         "-----",
                                         "-----"]]
                                  }
@@ -193,18 +223,25 @@ class Tetromino(pygame.sprite.Sprite):
             new_block = Block(self.colour, i)
             self.blocks.add(new_block)
 
+    def set_play_coordinates(self):
+        self.rect.x = random.choice(range(0, 8*self.block_size, self.block_size))
+        self.rect.y = 20
+        self.next_tetromino = False
+        self.current_tetromino = True
+
     def rotate(self):
-        if self.current_rotation == len(self.shape_dictionary.get(self.shape)) - 1:
-            self.current_rotation = 0
+        if not self.next_tetromino:
+            if self.current_rotation == len(self.shape_dictionary.get(self.shape)) - 1:
+                self.current_rotation = 0
+            else:
+                self.current_rotation += 1
         else:
-            self.current_rotation += 1
+            pass
 
     def gravity(self):
         self.rect.y += self.block_size
 
     def draw(self, surface):
-        self.gravity()
-
         x, y = self.rect.x, self.rect.y
         block_size = 40
         i = 0
@@ -226,11 +263,5 @@ if __name__ == "__main__":
 
     new_game = SetupGame()
 
-    tet = Tetromino()
-
     while True:
         new_game.loop()
-        new_game.event_handling()
-
-        tet.draw(new_game.game_display)
-
